311d55aacc23035f7ef45de01f9f75471dbddbbb
Евгений <@DESKTOP-SJVT4QK>
2022-05-19 14:47:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int progression = 7;
+            int minNumber = 5;
+            for (int i = 96; i>=minNumber; i-=progression ) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
